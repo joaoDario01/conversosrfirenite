a152a86dfc56f225fc65f4105b0621e6f2f86e3f
joaoDario01 <153123879+joaoDario01@users.noreply.github.com>
2023-12-06 18:38:12 -0300

    Add files via upload

---
+++ areaTriangulo.py
@@ -0,0 +1,9 @@
+#leitura
+print('digite a base do seu triangulo:')
+base = float(input())
+print('digite a altura do seu triangulo:')
+altura = float(input())
+#processamento
+area = (base * altura) / 2
+#saida
+print('a area do seu triamgulo é:', area)
