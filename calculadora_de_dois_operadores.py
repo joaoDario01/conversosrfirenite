2ec3ede78e2bdd5b18d674b8ebb29acdb74658a5
joaoDario01 <153123879+joaoDario01@users.noreply.github.com>
2023-12-08 12:52:36 -0300

    Add files via upload

---
+++ calculadora_de_dois_operadores.py
@@ -0,0 +1,48 @@
+#leitura da escolha
+print('escolha qual o tipo de operacao quer fazer')
+print('1 - somar')
+print('2 - subtrair')
+print('3 - multiplicar')
+print('4 - dividir')
+operacao = int(input())
+#processamento da escolha
+if operacao == 1:
+    #leitura
+    print('SOMA')
+    print('digite o primeiro valor')
+    operador1 = float(input())
+    print('digite o segundo valor')
+    operador2 = float(input())
+    #processamento
+    resultado = operador1 + operador2
+    print('o resultado e:', resultado)
+elif operacao == 2:
+    #leitura
+    print('SUBTRACAO')
+    print('digite o primeiro valor')
+    operador1 = float(input())
+    print('digite o segundo valor')
+    operador2 = float(input())
+    #processamento
+    resultado = operador1 - operador2
+    print('o resultado e:', resultado)
+elif operacao == 3:
+    #leitura
+    print('MULTIPLICACAO')
+    print('digite o primeiro valor')
+    operador1 = float(input())
+    print('digite o segundo valor')
+    operador2 = float(input())
+    # processamento
+    resultado = operador1 * operador2
+    print('o resultado e:', resultado)
+elif operacao == 4:
+    #leitura
+    print('DIVISAO')
+    print('digite o primeiro valor')
+    operador1 = float(input())
+    print('digite o segundo valor')
+    operador2 = float(input())
+    # processamento
+    resultado = operador1 / operador2
+    print('o resultado e:', resultado)
