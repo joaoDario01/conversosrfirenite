256a26062edc12bef998210bcc5301bd0ca071db
joaoDario01 <153123879+joaoDario01@users.noreply.github.com>
2023-12-11 01:07:40 -0300

    Add files via upload

---
+++ tabuada_repeticao.py
@@ -0,0 +1,6 @@
+#tabuada usando estrutura de repeticao
+fator1 = int(input('digite o numero que deseja descobrir a tabuada: '))
+fator2 = 1
+while fator2 <= 10:
+    print(fator1, 'x', fator2, '=', fator1*fator2 )
+    fator2 = fator2 + 1
